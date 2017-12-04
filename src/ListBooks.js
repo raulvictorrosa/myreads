7d878cc4b5953fa545ed86b124a557d1bc57c253
Raul Victor Rosa <raulvictorrosa@gmail.com>
2017-12-04 16:20:13 -0200

    feat: Change to a Functional Stateless Component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import BookShelf from './BookShelf'
 
-class ListBooks extends Component {
-  static propTypes = {
+const ListBooks = (props) => {
+  ListBooks.propTypes = {
     books: PropTypes.arrayOf(
       PropTypes.shape({
         authors: PropTypes.arrayOf(PropTypes.string.isRequired),
@@ -18,7 +18,7 @@ class ListBooks extends Component {
     onShelfChange: PropTypes.func.isRequired
   }
 
-  shelfs = [
+  const shelfs = [
     {
       slug: `currentlyReading`,
       heading: `Currently Reading`
@@ -33,34 +33,31 @@ class ListBooks extends Component {
     }
   ]
 
-  render() {
-    const { shelfs } = this
-    const { books, onShelfChange } = this.props
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {shelfs.map((shelf, index) => (
-              <BookShelf
-                books={books.filter((book) => book.shelf === shelf.slug)}
-                key={index}
-                title={shelf.heading}
-                onShelfChange={(id, shelf) => {
-                  onShelfChange(id, shelf)
-                }}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
+  const { books, onShelfChange } = props
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          {shelfs.map((shelf, index) => (
+            <BookShelf
+              books={books.filter((book) => book.shelf === shelf.slug)}
+              key={index}
+              title={shelf.heading}
+              onShelfChange={(id, shelf) => {
+                onShelfChange(id, shelf)
+              }}
+            />
+          ))}
         </div>
       </div>
-    )
-  }
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  )
 }
 
 export default ListBooks
